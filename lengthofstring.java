62791aaba9bc6a694508c6b16dff5ee8026932c2
vishnuvardhank21 <vishnuvardhankvv18@gmail.com>
2026-06-29 10:00:26 +0530

    Add files via upload

---
+++ lengthofstring.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+      String s = sc.nextLine();
+      int count = 0;
+      for(int i=0;i<s.length();i++){
+        count++;
+      }
+      System.out.println(count); 
+    }
+}
+
